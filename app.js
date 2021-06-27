df708077c72db931c8efa8914a29d03ce7546374
VolodymyrStyk <volodymyr.styk@gmail.com>
2021-06-27 23:26:29 +0300

    add postman requests

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const apiRouter = require('./routes');
+const {server} = require('./constants');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.use('/', apiRouter);
+
+app.listen(server.PORT, () => {
+    console.log(`App works: ${server.HOST}:${server.PORT}`);
+});
