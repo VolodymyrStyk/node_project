d8a29b5b3a3b03d65792f110bd93e8d8113be42c
VolodymyrStyk <volodymyr.styk@gmail.com>
2021-07-05 17:22:26 +0300

    feat: add checkUserRole for put and delete method.

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -15,15 +15,17 @@ router.use('/:userId', usersMiddleware.checkIsUserExist);
 router.get('/:userId',
   usersController.getUserById);
 
-router.put('/:userId',
-  usersMiddleware.checkAllDataValid,
-  usersMiddleware.checkEmailExist,
-  usersController.updateUser);
-
 router.patch('/:userId',
   usersMiddleware.checkSomeDataValid,
   usersMiddleware.checkEmailExist,
   usersController.updateSomeField);
+
+router.use('/:userId', usersMiddleware.checkUserRole(['admin']));
+
+router.put('/:userId',
+  usersMiddleware.checkAllDataValid,
+  usersMiddleware.checkEmailExist,
+  usersController.updateUser);
 
 router.delete('/:userId',
   usersController.deleteUserById);
