d203093aef0f055e0f055f663dbabe711e82d249
VolodymyrStyk <volodymyr.styk@gmail.com>
2021-07-13 02:38:50 +0300

    feat: implements delete by id

--- middlewares/user/checkAvatar.js
+++ middlewares/user/checkAvatar.js
@@ -3,11 +3,13 @@ const { ErrorHandler, errorMessages: { AVATAR_UPLOAD } } = require('../../errors
 
 module.exports = (req, res, next) => {
   try {
-    if (req.photos.length > 1) {
-      throw new ErrorHandler(statusCode.BAD_REQUEST, AVATAR_UPLOAD.message, AVATAR_UPLOAD.code);
-    }
+    if (req.photos) {
+      if (req.photos.length > 1) {
+        throw new ErrorHandler(statusCode.BAD_REQUEST, AVATAR_UPLOAD.message, AVATAR_UPLOAD.code);
+      }
 
-    [req.avatar] = req.photos;
+      [req.avatar] = req.photos;
+    }
 
     next();
   } catch (e) {
--- middlewares/user/uploadFiles.middleware.js
+++ middlewares/user/uploadFiles.middleware.js
@@ -13,40 +13,42 @@ const {
 
 module.exports = (req, res, next) => {
   try {
-    const files = Object.values(req.files);
-    const documents = [];
-    const videos = [];
-    const photos = [];
+    if (req.files) {
+      const files = Object.values(req.files);
+      const documents = [];
+      const videos = [];
+      const photos = [];
 
-    for (let i = 0; i < files.length; i++) {
-      const { name, size, mimetype } = files[i];
+      for (let i = 0; i < files.length; i++) {
+        const { name, size, mimetype } = files[i];
 
-      if (PHOTOS_MIMETYPES.includes(mimetype)) {
-        if (size > PHOTO_MAX_SIZE) {
-          throw ErrorHandler(statusCode.BAD_REQUEST, FILE_SIZE_ERROR.message(name), FILE_SIZE_ERROR.code);
+        if (PHOTOS_MIMETYPES.includes(mimetype)) {
+          if (size > PHOTO_MAX_SIZE) {
+            throw ErrorHandler(statusCode.BAD_REQUEST, FILE_SIZE_ERROR.message(name), FILE_SIZE_ERROR.code);
+          }
+
+          photos.push(files[i]);
+        } else if (VIDEOS_MIMETYPES.includes(mimetype)) {
+          if (size > VIDEO_MAX_SIZE) {
+            throw ErrorHandler(statusCode.BAD_REQUEST, FILE_SIZE_ERROR.message(name), FILE_SIZE_ERROR.code);
+          }
+
+          videos.push(files[i]);
+        } else if (DOCS_MIMETYPES.includes(mimetype)) {
+          if (size > DOCS_MAX_SIZE) {
+            throw ErrorHandler(statusCode.BAD_REQUEST, FILE_SIZE_ERROR.message(name), FILE_SIZE_ERROR.code);
+          }
+
+          documents.push(files[i]);
+        } else {
+          throw ErrorHandler(statusCode.BAD_REQUEST, WRONG_MIME_TYPE.message(name), WRONG_MIME_TYPE.code);
         }
-
-        photos.push(files[i]);
-      } else if (VIDEOS_MIMETYPES.includes(mimetype)) {
-        if (size > VIDEO_MAX_SIZE) {
-          throw ErrorHandler(statusCode.BAD_REQUEST, FILE_SIZE_ERROR.message(name), FILE_SIZE_ERROR.code);
-        }
-
-        videos.push(files[i]);
-      } else if (DOCS_MIMETYPES.includes(mimetype)) {
-        if (size > DOCS_MAX_SIZE) {
-          throw ErrorHandler(statusCode.BAD_REQUEST, FILE_SIZE_ERROR.message(name), FILE_SIZE_ERROR.code);
-        }
-
-        documents.push(files[i]);
-      } else {
-        throw ErrorHandler(statusCode.BAD_REQUEST, WRONG_MIME_TYPE.message(name), WRONG_MIME_TYPE.code);
       }
-    }
 
-    req.documents = documents;
-    req.videos = videos;
-    req.photos = photos;
+      req.documents = documents;
+      req.videos = videos;
+      req.photos = photos;
+    }
 
     next();
   } catch (e) {
